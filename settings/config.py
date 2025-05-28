32d5c248b40aa7c4c99ecfea5c6ff5fbc3d31fd4
Thiethv <hoangvanthiet1987@gmail.com>
2025-05-28 16:16:11 +0700

    three commit

---
+++ settings/config.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # Chỉ dùng cho local
+
+USER_NAME = os.getenv("USER_NAME")
+PASSWORD = os.getenv("PASSWORD")
+
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_API = os.getenv("SUPABASE_API")
+
+print(SUPABASE_URL, SUPABASE_API)
